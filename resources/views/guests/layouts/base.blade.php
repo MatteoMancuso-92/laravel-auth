2a9a626b755243add06c79de2726624a190a6b1d
MatteoMancuso-92 <Matteotrieste@outlook.com>
2023-07-06 16:53:08 +0200

    06-07-23-01

--- resources/views/guests/layouts/base.blade.php
+++ resources/views/guests/layouts/base.blade.php
@@ -4,18 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Documento</title>
     @vite('resources/js/app.js')
 </head>
 <body>
-    @include('admin.includes.header')
-
-    <div class="container">
-        <main>
-            @yield('contents')
-        </main>
-    </div>
-
-    @include('admin.includes.footer')
+    @include('guests.includes.header')
+        <div class="container">
+            <main>
+                @yield('contents')
+            </main>
+        </div>
+    @include('guests.includes.footer')
 </body>
 </html>
