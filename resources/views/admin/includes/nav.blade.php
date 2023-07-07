9d73721cd95aa4734f602fd6417a90270f42bd10
MatteoMancuso-92 <Matteotrieste@outlook.com>
2023-07-07 17:48:56 +0200

    07-07-23-02

--- resources/views/admin/includes/nav.blade.php
+++ resources/views/admin/includes/nav.blade.php
@@ -1,3 +1,5 @@
+@php $user = Auth::user(); @endphp
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('guests.home') }}">Boolpress</a>
@@ -21,6 +23,25 @@
                     </ul>
                 </li>
             </ul>
+
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ $user->name }}
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('admin.profile.edit') }}">Edit profile</a></li>
+
+                        <li>
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button>Logout</button>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
             {{-- <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
